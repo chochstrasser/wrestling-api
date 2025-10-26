967f021106536093d737776a4872118e0246b1d7
chochstrasser <chochstrasser@sofi.org>
2025-10-25 20:02:56 -0600

    make so init files based on errors

---
+++ app/scrappers/__init__.py
@@ -0,0 +1,1 @@
+# This file makes the scrappers directory a Python package
--- start.py
+++ start.py
@@ -1,6 +1,9 @@
 import os
-import uvicorn
 
 if __name__ == "__main__":
+    from app.main import app
+    import uvicorn
+
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    print(f"Starting server on 0.0.0.0:{port}")
+    uvicorn.run(app, host="0.0.0.0", port=port)
